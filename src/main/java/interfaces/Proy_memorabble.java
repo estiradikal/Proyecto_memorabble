40db91491e102236e681e430bdcf6c00f5f1abff
Estiven Martinez <estivenfrv@gmail.com>
2022-11-01 22:05:06 -0500

    Main del programa

---
+++ src/main/java/interfaces/Proy_memorabble.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package interfaces;
+
+/**
+ *
+ * @author ESTIVEN
+ */
+public class Proy_memorabble {
+
+    public static void main(String[] args) {
+        
+        interfaz_memorabble im=new interfaz_memorabble();
+        im.setVisible(true);
+                
+    }
+}
