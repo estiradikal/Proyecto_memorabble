aab60e1c181e487706a19f3dc2402face45d6d5c
Juan David Loaiza <juan.loaiza.santiago@correounivalle.edu.co>
2022-11-04 02:05:39 -0500

    Ajuste de la cabecera de comentario

--- src/interfaces/main.java
+++ src/interfaces/main.java
@@ -5,13 +5,13 @@ package interfaces;
     LABORATORIO  
     Profesor: Luis Romo Portilla
 
-    Archivo: Proy.java
+    Archivo: main.java
     Autores (A-Z): 
             * Alejandro Guerrero <alejandro.cano@correounivalle.edu.co> - 202179652-3743
             * Estiven Andres Martinez Granados <estiven.martinez@correounivalle.edu.co> - 202179687-3742
             * Juan David Loaiza Santiago <juan.loaiza.santiago@correounivalle.edu.co> - 2177570-3743             
     Fecha creación: 10-30-2022
-    Fecha última modificación: 10-30-2022
+    Fecha última modificación: 11-04-2022
     Versión: 0.1
     Licencia: GNU-GPL
 */
