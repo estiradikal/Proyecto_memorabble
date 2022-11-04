922402e6ddcd38fe078c2b1a21f9bafb2c19124d
Juan David Loaiza <juan.loaiza.santiago@correounivalle.edu.co>
2022-11-04 12:05:50 -0500

    Ahora inicia la interfaz_jugable con una puntuación de cero

--- src/interfaces/gameLauncher.java
+++ src/interfaces/gameLauncher.java
@@ -37,7 +37,7 @@ public class gameLauncher {
     }
     
     public void nuevoJuego(){       
-        interfaz_jugable ij = new interfaz_jugable(tiempoInicioHoras, tiempoInicioMinutos, tiempoInicioSegundos, 3, 0, 0, 1);
+        interfaz_jugable ij = new interfaz_jugable(tiempoInicioHoras, tiempoInicioMinutos, tiempoInicioSegundos, 3, 0, 0, 1, 0);
         ij.setVisible(true); 
     }
 }
