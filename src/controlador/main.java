03f528f66e1899d746626651c48811712c96cf96
Juan David Loaiza <juan.loaiza.santiago@correounivalle.edu.co>
2022-11-13 00:03:03 -0500

    Adicion de sonido

--- src/controlador/main.java
+++ src/controlador/main.java
@@ -1,9 +1,7 @@
 package controlador;
 
-
-
 import vista.interfaz_memorabble;
-
+import modelo.*;
 
 /*
     Fundamentos de programación orientada a eventos 750014C-01  
@@ -17,11 +15,13 @@ import vista.interfaz_memorabble;
             * Juan David Loaiza Santiago <juan.loaiza.santiago@correounivalle.edu.co> - 2177570-3743             
     Fecha creación: 10-30-2022
     Fecha última modificación: 11-04-2022
-    Versión: 0.1
+    Versión: 1.0
     Licencia: GNU-GPL
 */
 public class main {
-    public static void main(String[] args) {        
+    public static void main(String[] args) { 
+        reproductorSonido reproductor = new reproductorSonido();
+        reproductor.reproducirSonido("memorabble.wav");
         interfaz_memorabble im = new interfaz_memorabble();
         im.setVisible(true);  
     }
