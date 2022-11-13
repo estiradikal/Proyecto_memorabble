6406bab0d4aed785932df732e716210023ea54b6
Juan David Loaiza <juan.loaiza.santiago@correounivalle.edu.co>
2022-11-12 23:39:15 -0500

    Clase para reproducir sonidos

---
+++ src/modelo/reproductorSonido.java
@@ -0,0 +1,38 @@
+
+package modelo;
+
+import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.*;
+
+/*
+    Fundamentos de programación orientada a eventos 750014C-01  
+    LABORATORIO  
+    Profesor: Luis Romo Portilla
+
+    Archivo: reproductorSonido.java
+    Autores (A-Z): 
+            * Alejandro Guerrero <alejandro.cano@correounivalle.edu.co> - 202179652-3743
+            * Estiven Andres Martinez Granados <estiven.martinez@correounivalle.edu.co> - 202179687-3743
+            * Juan David Loaiza Santiago <juan.loaiza.santiago@correounivalle.edu.co> - 2177570-3743             
+    Fecha creación: 11-12-2022
+    Fecha última modificación: 11-12-2022 
+    Versión: 1.0
+    Licencia: GNU-GPL
+*/
+public class reproductorSonido{
+    
+    public reproductorSonido(){
+    }
+    
+    public void reproducirSonido(String nombreSonido){
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("src/sonido/"+nombreSonido).getAbsoluteFile());
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+        } catch(UnsupportedAudioFileException | IOException | LineUnavailableException ex) {
+         System.out.println("Error al reproducir el sonido de " + nombreSonido);   
+        }
+    }
+}
