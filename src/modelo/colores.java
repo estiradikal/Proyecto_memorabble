0a42d89b3040305756badf5edd56058b8d8e1b33
Juan David Loaiza <juan.loaiza.santiago@correounivalle.edu.co>
2022-11-13 22:30:55 -0500

    Documentación añadida

--- src/modelo/colores.java
+++ src/modelo/colores.java
@@ -16,7 +16,7 @@ import javax.swing.ImageIcon;
             * Estiven Andres Martinez Granados <estiven.martinez@correounivalle.edu.co> - 202179687-3743
             * Juan David Loaiza Santiago <juan.loaiza.santiago@correounivalle.edu.co> - 2177570-3743             
     Fecha creación: 11-07-2022
-    Fecha última modificación: 11-11-2022
+    Fecha última modificación: 11-13-2022
     Versión: 1.6
     Licencia: GNU-GPL
 */
@@ -33,12 +33,19 @@ public class colores{
         misColores.add("VERDE");
     }   
  
+    /**
+     * Selecciona un color random entre los que tiene en el vector misColores
+     */
     public String generarColor(){
         int numeroRandom;
         numeroRandom = (int) 0 + (int) (Math.random() * ((misColores.size() - 1) + 1));        
         return misColores.get(numeroRandom);
     }
     
+    /**
+     * Cambia el ImageIcon del JLabel que reciba por uno de criterio de color
+     * @param lbl_criterio El JLabel al que asignará/cambiará un ImageIcon
+     */
     public void generarCriterioColor(JLabel lbl_criterio){
         ImageIcon miImagen = new ImageIcon();
           
@@ -67,6 +74,12 @@ public class colores{
         lbl_criterio.setIcon(new ImageIcon(miImagen.getImage().getScaledInstance(170, 170, Image.SCALE_SMOOTH)));
     }
     
+    /**
+     * Cambia el ImageIcon del JButton que reciba por uno de una ficha aleatoria
+     * @param fichaActual El boton al que cambiará el nombre
+     * @param fichaActualImg El String donde copiara el nombre del color/forma
+     * @param criterioActual El criterio que considerará para asignar un color/forma
+     */
     public void asignarColor(JButton fichaActual, String fichaActualImg, String criterioActual){ 
         int numeroRandom;
         numeroRandom = (int) 1 + (int) (Math.random() * ((5 - 1) + 1)); 
